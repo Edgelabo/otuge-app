abdb5258e307772cab5c987be90365e83aac2ded
Edge <edge@nire.com>
2024-10-26 01:40:32 +0900

    タブのデザイン

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: "お告げAPP",
+          favicon: "favicon.svg",
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
